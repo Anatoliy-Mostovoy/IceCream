e60d5de8a83a1fc37d8350ac1615379ac792c7ae
Anatoliy Mostovoy <mostovoyanatoliy@gmail.com>
2021-02-19 12:38:25 +0200

    Updates

--- script.d573be0b.js
+++ script.d573be0b.js
@@ -117,83 +117,19 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"../node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
-var bundleURL = null;
-
-function getBundleURLCached() {
-  if (!bundleURL) {
-    bundleURL = getBundleURL();
-  }
-
-  return bundleURL;
-}
-
-function getBundleURL() {
-  // Attempt to find the URL of the current script and use that as the base URL
-  try {
-    throw new Error();
-  } catch (err) {
-    var matches = ('' + err.stack).match(/(https?|file|ftp|chrome-extension|moz-extension):\/\/[^)\n]+/g);
-
-    if (matches) {
-      return getBaseURL(matches[0]);
-    }
-  }
-
-  return '/';
-}
-
-function getBaseURL(url) {
-  return ('' + url).replace(/^((?:https?|file|ftp|chrome-extension|moz-extension):\/\/.+)\/[^/]+$/, '$1') + '/';
-}
-
-exports.getBundleURL = getBundleURLCached;
-exports.getBaseURL = getBaseURL;
-},{}],"../node_modules/parcel-bundler/src/builtins/css-loader.js":[function(require,module,exports) {
-var bundle = require('./bundle-url');
-
-function updateLink(link) {
-  var newLink = link.cloneNode();
-
-  newLink.onload = function () {
-    link.remove();
-  };
-
-  newLink.href = link.href.split('?')[0] + '?' + Date.now();
-  link.parentNode.insertBefore(newLink, link.nextSibling);
-}
-
-var cssTimeout = null;
-
-function reloadCSS() {
-  if (cssTimeout) {
-    return;
-  }
-
-  cssTimeout = setTimeout(function () {
-    var links = document.querySelectorAll('link[rel="stylesheet"]');
-
-    for (var i = 0; i < links.length; i++) {
-      if (bundle.getBaseURL(links[i].href) === bundle.getBundleURL()) {
-        updateLink(links[i]);
-      }
-    }
-
-    cssTimeout = null;
-  }, 50);
-}
-
-module.exports = reloadCSS;
-},{"./bundle-url":"../node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"sass/main.scss":[function(require,module,exports) {
-var reloadCSS = require('_css_loader');
-
-module.hot.dispose(reloadCSS);
-module.hot.accept(reloadCSS);
-},{"./../fonts/dm-sans-v6-latin-regular.eot":[["dm-sans-v6-latin-regular.b8eca754.eot","fonts/dm-sans-v6-latin-regular.eot"],"fonts/dm-sans-v6-latin-regular.eot"],"./../fonts/dm-sans-v6-latin-regular.woff2":[["dm-sans-v6-latin-regular.a45f7eae.woff2","fonts/dm-sans-v6-latin-regular.woff2"],"fonts/dm-sans-v6-latin-regular.woff2"],"./../fonts/dm-sans-v6-latin-regular.woff":[["dm-sans-v6-latin-regular.951d2140.woff","fonts/dm-sans-v6-latin-regular.woff"],"fonts/dm-sans-v6-latin-regular.woff"],"./../fonts/dm-sans-v6-latin-regular.ttf":[["dm-sans-v6-latin-regular.8114e003.ttf","fonts/dm-sans-v6-latin-regular.ttf"],"fonts/dm-sans-v6-latin-regular.ttf"],"./../fonts/dm-sans-v6-latin-regular.svg":[["dm-sans-v6-latin-regular.8e9c9c54.svg","fonts/dm-sans-v6-latin-regular.svg"],"fonts/dm-sans-v6-latin-regular.svg"],"./../fonts/dm-sans-v6-latin-500.eot":[["dm-sans-v6-latin-500.6760241d.eot","fonts/dm-sans-v6-latin-500.eot"],"fonts/dm-sans-v6-latin-500.eot"],"./../fonts/dm-sans-v6-latin-500.woff2":[["dm-sans-v6-latin-500.01a97bb0.woff2","fonts/dm-sans-v6-latin-500.woff2"],"fonts/dm-sans-v6-latin-500.woff2"],"./../fonts/dm-sans-v6-latin-500.woff":[["dm-sans-v6-latin-500.044febe0.woff","fonts/dm-sans-v6-latin-500.woff"],"fonts/dm-sans-v6-latin-500.woff"],"./../fonts/dm-sans-v6-latin-500.ttf":[["dm-sans-v6-latin-500.9e53fdac.ttf","fonts/dm-sans-v6-latin-500.ttf"],"fonts/dm-sans-v6-latin-500.ttf"],"./../fonts/dm-sans-v6-latin-500.svg":[["dm-sans-v6-latin-500.acfa4f65.svg","fonts/dm-sans-v6-latin-500.svg"],"fonts/dm-sans-v6-latin-500.svg"],"./../fonts/dm-sans-v6-latin-700.eot":[["dm-sans-v6-latin-700.f08a1f3b.eot","fonts/dm-sans-v6-latin-700.eot"],"fonts/dm-sans-v6-latin-700.eot"],"./../fonts/dm-sans-v6-latin-700.woff2":[["dm-sans-v6-latin-700.d929dea6.woff2","fonts/dm-sans-v6-latin-700.woff2"],"fonts/dm-sans-v6-latin-700.woff2"],"./../fonts/dm-sans-v6-latin-700.woff":[["dm-sans-v6-latin-700.42c96a41.woff","fonts/dm-sans-v6-latin-700.woff"],"fonts/dm-sans-v6-latin-700.woff"],"./../fonts/dm-sans-v6-latin-700.ttf":[["dm-sans-v6-latin-700.405f0335.ttf","fonts/dm-sans-v6-latin-700.ttf"],"fonts/dm-sans-v6-latin-700.ttf"],"./../fonts/dm-sans-v6-latin-700.svg":[["dm-sans-v6-latin-700.b4c46e03.svg","fonts/dm-sans-v6-latin-700.svg"],"fonts/dm-sans-v6-latin-700.svg"],"./../fonts/titan-one-v8-latin-regular.eot":[["titan-one-v8-latin-regular.5ecfba8a.eot","fonts/titan-one-v8-latin-regular.eot"],"fonts/titan-one-v8-latin-regular.eot"],"./../fonts/titan-one-v8-latin-regular.woff2":[["titan-one-v8-latin-regular.c7773417.woff2","fonts/titan-one-v8-latin-regular.woff2"],"fonts/titan-one-v8-latin-regular.woff2"],"./../fonts/titan-one-v8-latin-regular.woff":[["titan-one-v8-latin-regular.7f3e05d4.woff","fonts/titan-one-v8-latin-regular.woff"],"fonts/titan-one-v8-latin-regular.woff"],"./../fonts/titan-one-v8-latin-regular.ttf":[["titan-one-v8-latin-regular.289eb43c.ttf","fonts/titan-one-v8-latin-regular.ttf"],"fonts/titan-one-v8-latin-regular.ttf"],"./../fonts/titan-one-v8-latin-regular.svg":[["titan-one-v8-latin-regular.6dce54aa.svg","fonts/titan-one-v8-latin-regular.svg"],"fonts/titan-one-v8-latin-regular.svg"],"./../images/Mobile/sectionbg.png":[["sectionbg.a70e544a.png","images/Mobile/sectionbg.png"],"images/Mobile/sectionbg.png"],"./../images/Mobile/sectionbg@2x.png":[["sectionbg@2x.97fe5147.png","images/Mobile/sectionbg@2x.png"],"images/Mobile/sectionbg@2x.png"],"./../images/Desktop/sectionbg.png":[["sectionbg.8220547b.png","images/Desktop/sectionbg.png"],"images/Desktop/sectionbg.png"],"./../images/Desktop/sectionbg@2x.png":[["sectionbg@2x.07f19032.png","images/Desktop/sectionbg@2x.png"],"images/Desktop/sectionbg@2x.png"],"./../images/Mobile/big-iceCream.png":[["big-iceCream.046de9af.png","images/Mobile/big-iceCream.png"],"images/Mobile/big-iceCream.png"],"./../images/Mobile/big-iceCream@2x.png":[["big-iceCream@2x.d2d3f3a5.png","images/Mobile/big-iceCream@2x.png"],"images/Mobile/big-iceCream@2x.png"],"./../images/Tablet/big-iceCream.png":[["big-iceCream.a7dd36ee.png","images/Tablet/big-iceCream.png"],"images/Tablet/big-iceCream.png"],"./../images/Tablet/big-iceCream@2x.png":[["big-iceCream@2x.86127a31.png","images/Tablet/big-iceCream@2x.png"],"images/Tablet/big-iceCream@2x.png"],"./../images/Desktop/big-iceCream.png":[["big-iceCream.57e49fa5.png","images/Desktop/big-iceCream.png"],"images/Desktop/big-iceCream.png"],"./../images/Desktop/big-iceCream@2x.png":[["big-iceCream@2x.854640c7.png","images/Desktop/big-iceCream@2x.png"],"images/Desktop/big-iceCream@2x.png"],"./../images/Tablet/milk.png":[["milk.7bc38d43.png","images/Tablet/milk.png"],"images/Tablet/milk.png"],"./../images/Tablet/milk@2x.png":[["milk@2x.dd6c6e8f.png","images/Tablet/milk@2x.png"],"images/Tablet/milk@2x.png"],"./../images/Desktop/milk.png":[["milk.9d72b69f.png","images/Desktop/milk.png"],"images/Desktop/milk.png"],"./../images/Desktop/milk@2x.png":[["milk@2x.147f3d66.png","images/Desktop/milk@2x.png"],"images/Desktop/milk@2x.png"],"./../images/Tablet/tasty-girl.png":[["tasty-girl.e59eac91.png","images/Tablet/tasty-girl.png"],"images/Tablet/tasty-girl.png"],"./../images/Tablet/tasty-girl@2x.png":[["tasty-girl@2x.76b83a0f.png","images/Tablet/tasty-girl@2x.png"],"images/Tablet/tasty-girl@2x.png"],"./../images/Desktop/tasty-girl.png":[["tasty-girl.92886e39.png","images/Desktop/tasty-girl.png"],"images/Desktop/tasty-girl.png"],"./../images/Desktop/tasty-girl@2x.png":[["tasty-girl@2x.f2869d2a.png","images/Desktop/tasty-girl@2x.png"],"images/Desktop/tasty-girl@2x.png"],"/Users/tolik/Downloads/GoIT_blender_3/IceCream/src/images/quotes.svg":[["quotes.4d7be276.svg","images/quotes.svg"],"images/quotes.svg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"index.js":[function(require,module,exports) {
-"use strict";
-
-require("./sass/main.scss");
-},{"./sass/main.scss":"sass/main.scss"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+})({"js/script.js":[function(require,module,exports) {
+$(document).ready(function () {
+  $('.rev-slider').slick({
+    arrows: false,
+    dots: true,
+    adaptiveHeight: true,
+    slidesToShow: 1,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 2000
+  });
+});
+},{}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -397,5 +333,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","index.js"], null)
-//# sourceMappingURL=/src.e31bb0bc.js.map
+},{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","js/script.js"], null)
+//# sourceMappingURL=/script.d573be0b.js.map
